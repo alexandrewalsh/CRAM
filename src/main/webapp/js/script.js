57d4dbb965413e427f52ae835c5af9cd85220cca
Enrique Avina <rickyavina@icloud.com>
2020-07-23 23:53:23 +0000

    Changed timestamp format and converted response handler into a promise

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -12,16 +12,17 @@ function loadClient() {
               function(err) { console.error("Error loading GAPI client for API", err); });
 }
 
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function execute() {
-    gapi.client.youtube.captions.download({
-      "id": "VTGY74ABZGNwXQyjX50KfOz5Fm9xrJkzPbwNVQBbKVI=",
-      "tlang": "en",
-      "tfmt": "sbv"
-    }).then(function(response) {
-        // Handle the results here (response.result has the parsed body).
-        console.log("Response", response);
+function epoch(timestamp) {
+    const parts = timestamp.split(':');
+    var hours = parseInt(parts[0]);
+    var minutes = parseInt(parts[1]);
+    var seconds = parseInt(parts[2]);
+    var epochTime = hours*3600 + minutes*60 + seconds;
+    return epochTime;
+}
 
+function parseCaptions(response){
+    return new Promise((success, failure) => {
         var json = {
             url: 'dummy',
             captions: []
@@ -29,15 +30,19 @@ function execute() {
 
         let reader = new FileReader();
 
+        // read response line by line
         reader.onloadend = function(evt){
             lines = evt.target.result.split(/\r\n|\n|\r/);  
 
+            /* Line types
+                0: timestamp line
+                1: text line
+                2: empty line
+            */
             var lineType = 0;
             var caption = {};
 
             lines.forEach(function(line) {
-                console.log('line: ' + line);
-
                 if (lineType == 0) {    // timestamp
                     if (line === ""){ // likely eof
                         return;
@@ -45,17 +50,16 @@ function execute() {
                     const timestamps = line.split(',');
                     if (timestamps.length != 2) {
                         console.error('timestamp line malformatted: ' + line);
+                        failure('failed');
                         return;
                     }
-                    
-                    caption["startTime"] = timestamps[0];
-                    caption["endTime"] = timestamps[1];
+                    caption["startTime"] = epoch(timestamps[0]);
+                    caption["endTime"] = epoch(timestamps[1]);
+
                 } else if (lineType == 1) {   // text
                     caption["text"] = line;
-
                     json.captions.push(caption);
                     caption = {};
-                    // json += "\t\t\t'text': " + line + "\n\t\t},\n";
                 } else if (lineType == 2) {
                     // do nothing 
                 }
@@ -65,16 +69,34 @@ function execute() {
                     lineType = 0;
                 }
             });
-            
-
+                
+            // send to backend
             console.log("stringy", JSON.stringify(json));
+            success(JSON.stringify(json));
+            // return JSON.stringify(json);
         };
 
         reader.readAsText(new Blob([response.body], {
-            type: 'text/plain'
+         type: 'text/plain'
         }));
-    },
-    function(err) { console.error("Execute error", err); });
+
+    });
+}
+
+
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function execute() {
+    gapi.client.youtube.captions.download({
+      "id": "VTGY74ABZGNwXQyjX50KfOz5Fm9xrJkzPbwNVQBbKVI=",
+      "tlang": "en",
+      "tfmt": "sbv"
+    }).then(function(response){
+        parseCaptions(response).then(json => {
+            console.log('recieved json', json);    
+        });
+    }, function(err) { 
+        console.error("Execute error", err); 
+    });
 
     // return gapi.client.youtube.captions.list({
     //   "videoId": "ovJcsL7vyrk",
