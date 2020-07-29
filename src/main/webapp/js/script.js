32d05f874873971179e6f8d2b64a72a426bccd5d
Enrique Avina <rickyavina@icloud.com>
2020-07-29 16:51:15 +0000

    Added search bar

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -197,6 +197,39 @@ function getIdFromUrl(url) {
     return video_id;
 }
 
+/**
+ * Toggle the search bar appearance based on event
+ * @param obj - the button invoking the click
+ * @param evt - the click event
+ */
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+
+    if(!container.hasClass('active')){
+        container.addClass('active');
+        evt.preventDefault();
+    }
+    else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+        container.removeClass('active');
+        // clear input
+        container.find('.search-input').val('');
+        // clear and hide result container when we press close
+        container.find('.result-container').fadeOut(100, function(){$(this).empty();});
+    }
+}
+
+/**
+ * Event handler for search bar query
+ * @param obj - the button invoking the click
+ * @param evt - the click event
+ */
+function submitFn(obj, evt){
+    $("#yess").addClass('search-wrapper-active');
+    value = $(obj).find('.search-input').val().trim();
+    evt.preventDefault();
+    execute(value);
+}
+
 var player;
 
 /**
