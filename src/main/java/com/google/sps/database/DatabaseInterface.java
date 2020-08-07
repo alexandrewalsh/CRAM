d92e70cdd026de6a129c384bac7e346ad922f25a
Alexandre Walsh <alexandrew@ucla.edu>
2020-08-07 00:25:42 +0000

    first round database implementation

--- src/main/java/com/google/sps/database/DatabaseInterface.java
+++ src/main/java/com/google/sps/database/DatabaseInterface.java
@@ -1,6 +1,10 @@
-
+/*
+ * Interface for DatabaseImpl class. Use as reference and official description of public functions.
+ */
 
 package com.google.sps.database;
+
+import java.util.*;
 
 public interface DatabaseInterface {
 
@@ -10,7 +14,7 @@ public interface DatabaseInterface {
      * @param metadata      String containing any metadata to be stored about the video
      * @return              Error code (0 upon success)
      */
-    int addVideo(String videoID, String metadata);
+    public int addVideo(String videoID, String metadata);
 
     /*
      * add a keyphrase + timestamp pair to a particular video in the db
@@ -19,15 +23,15 @@ public interface DatabaseInterface {
      * @param timestamps    List of timestamps corresponding to the entity's appearances in the video
      * @return              Error code (0 upon success, -1 if videoID not in db)
      */
-    int addClause(String videoID, String keyword, List<long> timestamps);
+    public int addClause(String videoID, String keyword, List<Long> timestamps);
 
     /*
-     * add a multiple keyphrase + timestamp pair to a particular video's entry
+     * add multiple keyphrase + timestamp pairs to a particular video's entry
      * @param videoID       Youtube ID of the video already in the db
      * @param clauses       map containing pairs of keys & corresponding timestamps
      * @return              Error code (0 upon success, -1 if videoID not in db)
      */
-    int addClauses(String videoID, Map<String, List<long>> clauses);
+    public int addClauses(String videoID, Map<String, List<Long>> clauses);
 
     /*
      * add metadata information to a particular video in the db
@@ -36,7 +40,14 @@ public interface DatabaseInterface {
      * @param overwrite     if true, any current metadata will be overwritten (otherwise, append)
      * @return              Error code (0 upon success, -1 if videoID not in db)
      */
-    int addMetadata(String videoID, String metadata, boolean overwrite);
+    public int addMetadata(String videoID, String metadata, boolean overwrite);
+
+    /*
+     * retrieve all keywords + their timestamps in a specified videoID
+     * @param videoID       Youtube ID of the video already in the db
+     * @return              Map of keyword + timestamp pairs belonging to videoID (null if videoID not in db)
+     */
+    public Map<String, List<Long>> getAllKeywords(String videoID);
 
     /*
      * retrieve specific timestamps for a specified keyword belonging to videoID
@@ -44,13 +55,7 @@ public interface DatabaseInterface {
      * @param keyword       Keyphrase in the videoID's db entry
      * @return              List of timestamps representing the times that keyword appears in videoID (null if videoID not in db or keyword not in videoID)
      */
-    List<long> getTimesForKeyword(String videoID, String keyword);
+    public List<Long> getTimesForKeyword(String videoID, String keyword);
 
-    /*
-     * retrieve all entities + their timestamps in a specified videoID
-     * @param videoID       Youtube ID of the video already in the db
-     * @return              Map of keyword + timestamp pairs belonging to videoID (null if videoID not in db)
-     */
-    Map<String, List<long>> getAllEntities(String videoID);
 }
   
