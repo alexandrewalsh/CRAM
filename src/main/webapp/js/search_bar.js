a3667ce9876b36ab1fc1fa5f861a5d14c5341356
Nathan Zhao <90603yahoo@gmail.com>
2020-08-12 16:47:22 +0000

    add comments to comparator methods

--- src/main/webapp/js/search_bar.js
+++ src/main/webapp/js/search_bar.js
@@ -19,14 +19,21 @@ function searchToggle(obj, evt){
     }
 }
 
-function entitySearchSubmit() {
-    console.log("Submit Button pushed");
-    return;
-}
 
+/**
+ * Comparator function that compares two entities alphabetically
+ * @param a - a <td> tag object to compare the text of
+ * @param b - a <td> tag object to compare the text of
+ * @return  - positive if 'a' is larger than 'b'
+ *          - negative if 'a' is smaller than 'b'
+ *          - zero if 'a' equals 'b'
+ */
 function compareEntities(a, b) {
+    // Gets the text from the tag
     var aText = $(a).text();
     var bText = $(b).text();
+
+    // Returns numerical values based on the comparison of the texts
     if (aText > bText) {
         return 1;
     } 
@@ -36,17 +43,24 @@ function compareEntities(a, b) {
     return 0;
 }
 
+
+/**
+ * Comparator function that compares two entities chronologically by timestamps
+ * @param a - a <td> tag object to compare the text timestamps of
+ * @param b - a <td> tag object to compare the text timestamps of
+ * @return  - positive if 'a' is larger than 'b'
+ *          - negative if 'a' is smaller than 'b'
+ *          - zero if 'a' equals 'b'
+ */
 function compareTimestamps(a, b) {
+    // Gets the text and timestamp length
     var aText = $(a).text();
     var bText = $(b).text();
     var aList = timestamps[aText];
     var bList = timestamps[bText];
-    console.log("NEW");
-    console.log(aText);
-    console.log(bText);
 
+    // Iterates through the lists up until the shorter length, checking the values in order
     var limit = (aList.length > bList.length) ? bList.length : aList.length;
-    console.log(limit);
     for (var i = 0; i < limit; i++) {
         if (aList[i] > bList[i]) {
             return 1;
@@ -55,18 +69,23 @@ function compareTimestamps(a, b) {
             return -1;
         } 
     }
+
+    // If all contents are equal up until the limit, the shorter list will preceed the longer list
     if (aList.length > bList.length) {
         return 1;
     }
     if (aList.length < bList.length) {
         return -1;
     }
+
+    // If the lists are equal, we sort alphabetically 
     return compareEntities(a, b);
 }
 
 
 $(document).ready(function() {
 
+    // Searchbar to filter entities 
     $('#entity-seachbar').on('keyup', function() {
         var query = $(this).val().toLowerCase();
         var entities = $('#output table tr td:first-child');
@@ -86,16 +105,18 @@ $(document).ready(function() {
         });
     });
 
+    // Select to sort entities
     $('#entity-sort').change(() => {
         var optionSelected = $("option:selected", this).val();
         console.log(optionSelected)
         if (optionSelected == 'alphabetical') {
             var list = $('#output table tr td:first-child').sort(compareEntities).map((i, elem) => elem.textContent);
             $('#output').html(styleEntitiesFromList(list));
-        }
-        if (optionSelected == 'chronological') {
+        } else if (optionSelected == 'chronological') {
             var list = $('#output table tr td:first-child').sort(compareTimestamps).map((i, elem) => elem.textContent);
             $('#output').html(styleEntitiesFromList(list));      
+        } else {
+            return;
         }
         setClickableEntities();
         setClickableTimestamps();
