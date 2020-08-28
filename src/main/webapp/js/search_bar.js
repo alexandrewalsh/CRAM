37d9915c5e520a3a7cda098e76ff8a896608c78b
Nathan Zhao <90603yahoo@gmail.com>
2020-08-28 22:41:25 +0000

    cannot submit queries when not on query page section

--- src/main/webapp/js/search_bar.js
+++ src/main/webapp/js/search_bar.js
@@ -315,6 +315,7 @@ function showSelectedSection(selected) {
             $('#bookmarks-toggle-button').removeClass('active-tab');
             $('#keywords-output table tr').show();
             $('.content').css('maxHeight', '0px');
+            $('#entity-search-form').attr('onsubmit', 'return false;');
             break;
         case 'query':
             $('#keywords-output').hide();
@@ -324,6 +325,7 @@ function showSelectedSection(selected) {
             $('#bookmarks-output').hide();
             $('#bookmarks-toggle-button').removeClass('active-tab');
             $('.content').css('maxHeight', '0px');
+            $('#entity-search-form').attr('onsubmit', 'getSearchResults(this, event);');
             break;
         case 'bookmarks':
             $('#keywords-output').hide();
@@ -333,6 +335,7 @@ function showSelectedSection(selected) {
             $('#bookmarks-output').show();
             $('#bookmarks-toggle-button').addClass('active-tab');
             $('#bookmarks-output ul li').show();
+            $('#entity-search-form').attr('onsubmit', 'return false;');
             break;
     }
 
@@ -346,9 +349,6 @@ function showSelectedSection(selected) {
  * @param evt - the click event
  */
  function getSearchResults(obj, evt) {
-    if ($('#query-output').css('dispaly') == 'none') {
-        return;
-    }
     evt.preventDefault();
     $('#query-output table').remove();
     $('#query-loading-text').show();
@@ -357,7 +357,7 @@ function showSelectedSection(selected) {
 
     if (ytCaptions == "") {
         console.log("YT Captions not yet set!");
-        return;
+        return false;
     }
 
     // Here goes the post request
