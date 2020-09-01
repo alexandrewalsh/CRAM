ec0a5069bd0a81c8316a8101d734388a6928f9eb
Nathan Zhao <90603yahoo@gmail.com>
2020-09-01 20:20:37 +0000

    remove unnecessary console logs

--- src/main/webapp/js/gensim.js
+++ src/main/webapp/js/gensim.js
@@ -10,8 +10,6 @@
  */
 async function postGensim(url, v_id, data, callback=handleGensimResponse) {
     console.log("POST gesim request to " + url + "...")
-    console.log(v_id);
-    console.log(data);
     try {
         const response = await fetch(url, {
             method: 'POST',
